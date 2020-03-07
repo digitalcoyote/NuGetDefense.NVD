b65cb13555b5c4edde0cf84677671065390088b8
Curtis Carter <lokiphasma@gmail.com>
2020-03-07 07:43:40 -0600

    Splitting NVD Into a separate Repo
    splitting it out to make it easier to automate builds/updates.

--- Src/NuGetDefense/Configuration/BlacklistedPackage.cs
+++
@@ -1,8 +0,0 @@
-namespace NuGetDefense.Configuration
-{
-    public class BlacklistedPackage
-    {
-        public NuGetPackage Package { get; set; }
-        public string CustomErrorMessage { get; set; }
-    }
-}
