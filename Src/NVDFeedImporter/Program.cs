acd418a95662cbe92e3f6decba13e5599a795d2a
Curtis Carter <lokiphasma@gmail.com>
2020-06-28 12:50:57 -0500

    Ability to Specify Output Path

--- Src/NVDFeedImporter/Program.cs
+++ Src/NVDFeedImporter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using NuGet.Versioning;
 using NuGetDefense;
@@ -13,11 +14,13 @@ namespace NVDFeedImporter
 
         private static async Task Main(string[] args)
         {
+            var outPath = BinName;
+            if (args.Length > 0) outPath = Path.Combine(args[0], BinName);
             var vulnDict =
                 new Dictionary<string, Dictionary<string, VulnerabilityEntry>>();
             await foreach (var feed in FeedUpdater.GetFeedsAsync())
                 FeedUpdater.AddFeedToVulnerabilityData(feed, vulnDict);
-            VulnerabilityData.SaveToBinFile(vulnDict, BinName, TimeSpan.FromMinutes(1));
+            VulnerabilityData.SaveToBinFile(vulnDict, outPath, TimeSpan.FromMinutes(1));
         }
     }
 }
